6d504482e09c86616ddb3bf33897d30d8f04ccfc
Andrew Madden <andrewmadden@catalyst-au.net>
2021-11-08 17:42:05 +1100

    MDL-72188 quizaccess_seb: Implement Safe Exam Browser JS API.
    * Replaces checking header for SEB config keys to assess quiz access.
    * Adds new web service accessible via Ajax
    * Forces the use of the new API where available
    * Stores access in Moodle SESSION for quiz instead of checking every
    page.

---
+++ mod/quiz/accessrule/seb/amd/src/view.js
@@ -0,0 +1,84 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Manage the quiz views.
+ *
+ * @module     quizaccess_seb/view
+ * @author     Andrew Madden <andrewmadden@catalyst-au.net>
+ * @copyright  2021 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+import Notification from "core/notification";
+import * as Str from "core/str";
+import * as Templates from "core/templates";
+
+/** @var SELECTOR List of CSS selectors. */
+const SELECTOR = {
+    MAIN: '#region-main',
+    LOADING: '.seb-loading',
+};
+
+/** @var Template List of mustache templates. */
+const TEMPLATE = {
+    LOADING: 'quizaccess_seb/loading',
+};
+
+/**
+ * Manages view when access has been granted.
+ */
+export const allowAccess = () => {
+    window.location.reload();
+};
+
+/**
+ * Manages view when access has been prevented.
+ */
+export const preventAccess = () => {
+    // Give information to end user why access was prevented.
+    Str.get_strings([
+        {key: 'invalidkeys', component: 'quizaccess_seb'},
+    ])
+    .then((Strings) => {
+        Notification.addNotification({
+            message: Strings[0],
+            type: "error"
+        });
+        return Strings;
+    }).catch(Notification.exception);
+};
+
+/**
+ * Add an alert to page to inform that Safe Exam Browser access is being checked.
+ *
+ * @return {Promise}
+ */
+export const addLoadingAlert = () => {
+    return Templates.render(TEMPLATE.LOADING, {}).then((html, js) => {
+        const alertRegion = window.document.querySelector(SELECTOR.MAIN);
+        return Templates.prependNodeContents(alertRegion, html, js);
+    }).catch(Notification.exception);
+};
+
+/**
+ * Remove the Safe Exam Browser access check alert from the page.
+ */
+export const clearLoadingAlert = () => {
+    const alert = window.document.querySelector(SELECTOR.LOADING);
+    if (alert) {
+        Templates.replaceNode(alert, '', '');
+    }
+};
