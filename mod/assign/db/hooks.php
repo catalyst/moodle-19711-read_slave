52709436b965628213076b2be8cc15bfe879c415
Nathan Nguyen <nathannguyen@catalyst-au.net>
2024-08-28 17:12:23 +1000

    MDL-82120 mod_assign: recalculate penalty

--- mod/assign/db/hooks.php
+++ mod/assign/db/hooks.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,18 +12,21 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * Version information
+ * Hooks listeners.
  *
  * @package    mod_assign
- * @copyright 2012 NetSpot {@link http://www.netspot.com.au}
+ * @copyright  2024 Catalyst IT Australia
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'mod_assign'; // Full name of the plugin (used for diagnostics).
-$plugin->version  = 2024100702;    // The current module version (Date: YYYYMMDDXX).
-$plugin->requires = 2024100100;    // Requires this Moodle version.
+$callbacks = [
+    [
+        'hook' => \core_grades\hook\before_penalty_recalculation::class,
+        'callback' => \mod_assign\hook\hook_callbacks::class . '::extend_penalty_recalculation',
+    ],
+];
